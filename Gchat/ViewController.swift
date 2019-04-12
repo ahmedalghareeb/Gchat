51307e3acda42f876bb449b44154e927badf6067
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2019-04-12 18:53:11 -0400

    Initial Commit

---
+++ Gchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gchat
+//
+//  Created by Ahmed Ghareeb on 2019-04-12.
+//  Copyright © 2019 Ahmed Ghareeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
